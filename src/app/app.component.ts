d0c72e14c1188724f09191df72e90cb4be4c4a84
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-06-29 01:22:06 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cards';
+    posts = [
+        {
+            title:"Neat Tree",
+            username: "billu",
+            content: "This is an awesome tree!",
+            imageUrl: "assets/tree.jpeg"
+        },
+        {
+            title:"Snowy Mountain",
+            username: "tillu",
+            content: "It's chilly over here!",
+            imageUrl: "assets/mountain.jpeg"
+        },
+        {
+            title:"Mountain Biking",
+            username: "gullu",
+            content: "Biking here is fun!",
+            imageUrl: "assets/biking.jpeg"
+        }
+    ];
+
+
+
 }
